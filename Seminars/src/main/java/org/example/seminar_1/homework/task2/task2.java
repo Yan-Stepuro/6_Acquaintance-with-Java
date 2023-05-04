ee36fe0e8eef4d0622ebfbc00e4320b22d4246f6
yanst <stepuro.yan@gmail.com>
2023-05-04 16:32:20 +0300

    add task2 to homework1

--- Seminars/src/main/java/org/example/seminar_1/homework/task2/task2.java
+++ Seminars/src/main/java/org/example/seminar_1/homework/task2/task2.java
@@ -8,5 +8,30 @@ package org.example.seminar1_homework.task2;
 //        3 -1 переход -> 3 в сумму
 //        Пример вывода: 5
 
+import java.util.Scanner;
+
 public class task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(sumDiff(scanner));
+    }
+
+    /**
+     *
+     * @param scanner - сканнер
+     * @return вывести сумму чисел, после которых идет отрицательное число
+     */
+    public static int sumDiff(Scanner scanner) {
+        int sum = 0;
+        int a = scanner.nextInt();
+
+        while (a != 0) {
+            int b = scanner.nextInt();
+            if (a > 0 && b < 0) {
+                sum += a;
+            }
+            a = b;
+        }
+        return sum;
+    }
 }
