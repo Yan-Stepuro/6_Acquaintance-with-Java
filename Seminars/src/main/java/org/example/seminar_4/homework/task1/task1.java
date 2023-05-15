bb0d1cba9672f4ba00ccb259d6f06d5f522cf549
yanst <stepuro.yan@gmail.com>
2023-05-15 19:12:40 +0300

    add homework to seminar 4

--- Seminars/src/main/java/org/example/seminar_4/homework/task1/task1.java
+++ Seminars/src/main/java/org/example/seminar_4/homework/task1/task1.java
@@ -1,2 +1,30 @@
-package org.example.seminar_4.homework.task1;public class task1 {
+package org.example.seminar_4.homework.task1;
+
+//1)Вывести список на экран в перевернутом виде (без массивов и ArrayList)
+//Пример:
+//        1 -> 2->3->4
+//        Вывод:
+//        4->3->2->1
+
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class task1 {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите размер списка: ");
+        int listSize = sc.nextInt();
+
+        for (int i = 0; i < listSize; i++) {
+            list.add(sc.nextInt());
+        }
+
+
+
+        for (int i = list.size() - 1; i >= 0 ; i--) {
+            System.out.println(list.get(i));
+        }
+    }
 }
--- Seminars/src/main/java/org/example/seminar_4/homework/task2/task2.java
+++ Seminars/src/main/java/org/example/seminar_4/homework/task2/task2.java
@@ -1,4 +1,70 @@
 package org.example.seminar_4.homework.task2;
 
+//Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+//
+//An input string is valid if:
+//
+//Open brackets must be closed by the same type of brackets.
+//Open brackets must be closed in the correct order.
+//Every close bracket has a corresponding open bracket of the same type.
+
+//Input: s = "()"
+//Output: true
+//
+//Input: s = "()[]{}"
+//Output: true
+//
+//Input: s = "(]"
+//Output: false
+
+import java.util.Scanner;
+import java.util.Stack;
+
 public class task2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input characters: ");
+        String s = sc.nextLine();
+        Boolean checkInput = true;
+
+        System.out.println("Input: " + s);
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != '(' && s.charAt(i) != ')' && s.charAt(i) != '{' && s.charAt(i) != '}' && s.charAt(i) != '[' && s.charAt(i) != ']') {
+                checkInput = false;
+            }
+        }
+
+        if (checkInput == true) {
+            System.out.println("Output: " + isValid(s));
+        }
+        else {
+            System.out.println("Output: Invalid input");
+        }
+    }
+
+    public static Boolean isValid(String s) {
+        Stack<Character> openBr = new Stack<>();
+
+        for (char item : s.toCharArray()) {
+            if (item == '(' || item == '{' || item == '[') {
+                openBr.push(item);
+            }
+            else if (item == ')' && !openBr.isEmpty() && openBr.peek() == '(') {
+                openBr.pop();
+            }
+            else if (item == '}' && !openBr.isEmpty() && openBr.peek() == '{') {
+                openBr.pop();
+            }
+            else if (item == ']' && !openBr.isEmpty() && openBr.peek() == '[') {
+                openBr.pop();
+            }
+            else {
+                return false;
+            }
+        }
+
+        return openBr.isEmpty();
+
+    }
 }
