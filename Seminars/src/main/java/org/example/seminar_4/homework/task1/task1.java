58692167754dc275b4b5f756d042ee1809086cf5
yanst <stepuro.yan@gmail.com>
2023-05-15 19:22:00 +0300

    add homework to seminar 4

--- Seminars/src/main/java/org/example/seminar_4/homework/task1/task1.java
+++ Seminars/src/main/java/org/example/seminar_4/homework/task1/task1.java
@@ -21,8 +21,6 @@ public class task1 {
             list.add(sc.nextInt());
         }
 
-
-
         for (int i = list.size() - 1; i >= 0 ; i--) {
             System.out.println(list.get(i));
         }
