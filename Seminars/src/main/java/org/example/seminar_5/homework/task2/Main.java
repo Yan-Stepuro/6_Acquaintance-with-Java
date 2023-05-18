9f59bf8935d34fd18780350de07963a06b02cf3c
yanst <stepuro.yan@gmail.com>
2023-05-18 19:37:01 +0300

    add homework to seminar_5

--- Seminars/src/main/java/org/example/seminar_5/homework/task2/Main.java
+++ Seminars/src/main/java/org/example/seminar_5/homework/task2/Main.java
@@ -21,8 +21,35 @@ package org.example.seminar_5.homework.task2;
 //        Иван Ежов
 //Написать программу, которая найдёт и выведет повторяющиеся имена с количеством повторений. Отсортировать по убыванию популярности.
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main { // доделать до 18.05
     public static void main(String[] args) {
+        String str =
+                "Иван Иванов\n" +
+                "Светлана Петрова\n" +
+                "Кристина Белова\n" +
+                "Анна Мусина\n" +
+                "Анна Крутова\n" +
+                "Иван Юрин\n" +
+                "Петр Лыков\n" +
+                "Павел Чернов\n" +
+                "Петр Чернышов\n" +
+                "Мария Федорова\n" +
+                "Марина Светлова\n" +
+                "Мария Савина\n" +
+                "Мария Рыкова\n" +
+                "Марина Лугова\n" +
+                "Анна Владимирова\n" +
+                "Иван Мечников\n" +
+                "Петр Петин\n" +
+                "Иван Ежов";
 
+        String[] names = str.split("\n");
+
+
+        System.out.println(str);
+        System.out.println(Arrays.asList(names));
     }
 }
